e1dd371edcbc1aaea2ee3846a4c0914cd76dc115
Piotr Kalota <piotrkalota99@gmail.com>
2021-10-24 19:19:58 +0200

    Add camera permission checking

--- app/src/main/java/com/example/alertmeapp/AlertFormActivity.java
+++ app/src/main/java/com/example/alertmeapp/AlertFormActivity.java
@@ -1,5 +1,6 @@
 package com.example.alertmeapp;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
@@ -9,15 +10,19 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AlertFormActivity extends AppCompatActivity {
+    private static final int CAMERA_REQUEST_CODE = 100;
+
     private ImageView takenPhoto;
 
     private final ActivityResultLauncher<Intent> mediaStoreActivityResultLauncher = registerForActivityResult(
@@ -37,12 +42,28 @@ public class AlertFormActivity extends AppCompatActivity {
             return;
         }
 
+        if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.CAMERA}, CAMERA_REQUEST_CODE);
+        }
+
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         mediaStoreActivityResultLauncher.launch(intent);
     }
 
     private boolean checkCameraHardware(Context context) {
         return context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == CAMERA_REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //TODO: on camera permission granted
+            } else {
+                //TODO: on camera permission denied
+            }
+        }
     }
 
     private void processTakenPhoto(ActivityResult result) {
