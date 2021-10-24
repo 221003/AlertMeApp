bc5d6e31afa19b58d8b8918b7483446749078581
Piotr Kalota <piotrkalota99@gmail.com>
2021-10-24 19:51:17 +0200

    Add choosing photo from gallery

--- app/src/main/java/com/example/alertmeapp/AlertFormActivity.java
+++ app/src/main/java/com/example/alertmeapp/AlertFormActivity.java
@@ -5,7 +5,10 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
@@ -19,15 +22,29 @@ import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 public class AlertFormActivity extends AppCompatActivity {
     private static final int CAMERA_REQUEST_CODE = 100;
+    private static final int GALLERY_REQUEST_CODE = 101;
+    private static final int REQUEST_EXTERNAL_STORAGE = 102;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+    private static String[] PERMISSIONS_CAMERA = {
+            Manifest.permission.CAMERA
+    };
 
     private ImageView takenPhoto;
 
-    private final ActivityResultLauncher<Intent> mediaStoreActivityResultLauncher = registerForActivityResult(
+    private final ActivityResultLauncher<Intent> cameraActivityResultLauncher = registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(),
             result -> processTakenPhoto(result));
+
+    private final ActivityResultLauncher<Intent> externalStorageActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> processChosenPhoto(result));
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,33 +53,63 @@ public class AlertFormActivity extends AppCompatActivity {
         takenPhoto = findViewById(R.id.takenPhotoImage);
     }
 
-    public void onBtnCameraClick(View view) {
+    public void onChoosePhotoClick(View view) {
+        if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(PERMISSIONS_STORAGE, REQUEST_EXTERNAL_STORAGE);
+        }
+
+        Intent intent = new Intent(
+                Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        externalStorageActivityResultLauncher.launch(intent);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == GALLERY_REQUEST_CODE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+            takenPhoto.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+        }
+    }
+
+    public void onTakePhotoClick(View view) {
         if (!checkCameraHardware(view.getContext())) {
             //TODO: inform that system has no camera
             return;
         }
 
         if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(new String[]{Manifest.permission.CAMERA}, CAMERA_REQUEST_CODE);
+            requestPermissions(PERMISSIONS_CAMERA, CAMERA_REQUEST_CODE);
         }
 
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        mediaStoreActivityResultLauncher.launch(intent);
+        cameraActivityResultLauncher.launch(intent);
     }
 
     private boolean checkCameraHardware(Context context) {
         return context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA);
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == CAMERA_REQUEST_CODE) {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                //TODO: on camera permission granted
-            } else {
-                //TODO: on camera permission denied
-            }
+    private void processChosenPhoto(ActivityResult result) {
+        if (result.getResultCode() == Activity.RESULT_OK) {
+            Intent data = result.getData();
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+            takenPhoto.setImageBitmap(BitmapFactory.decodeFile(picturePath));
         }
     }
 
@@ -72,6 +119,24 @@ public class AlertFormActivity extends AppCompatActivity {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
             takenPhoto.setImageBitmap(imageBitmap);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == CAMERA_REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //TODO: Camera permission granted
+            } else {
+                //TODO: Camera permission denied
+            }
+        } else if (requestCode == REQUEST_EXTERNAL_STORAGE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //TODO: External storage permission granted
+            } else {
+                //TODO: External storage permission denied
+            }
         }
     }
 }
