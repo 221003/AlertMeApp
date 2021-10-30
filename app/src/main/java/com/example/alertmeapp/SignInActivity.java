b0d4f22aca64aff9513e2de4413a513e93c4fc18
majkelw <michalwik@vp.pl>
2021-10-31 00:16:42 +0200

    Added handling error messages from server for the sign in form

--- app/src/main/java/com/example/alertmeapp/SignInActivity.java
+++ app/src/main/java/com/example/alertmeapp/SignInActivity.java
@@ -11,6 +11,7 @@ import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,6 +19,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.alertmeapp.api.AlertMeService;
 import com.example.alertmeapp.api.LoginBody;
 import com.example.alertmeapp.api.RestAdapter;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+
+import java.io.IOException;
 
 import okhttp3.ResponseBody;
 import retrofit2.Call;
@@ -27,10 +33,13 @@ import retrofit2.Response;
 public class SignInActivity extends AppCompatActivity {
     private final String INVALID_EMAIL = "Email invalid";
     //TODO: provide useful error message for invalid password
-    private final String INVALID_PASSWORD = "Password invalid";
-    private final String SIGN_UP_INFO =  "Or sign up here";
+    private final String EMPTY_PASSWORD = "Empty password field";
+    private final String EMPTY_EMAIL = "Empty email field";
+    private final String SIGN_UP_INFO = "Or sign up here";
     private final int INT_START = 11;
     private final int INT_END = 15;
+    private final int INCORRECT_PASSWORD_CODE = 10;
+    private final int INCORRECT_LOGIN_CODE = 11;
     private Object TextPaint;
 
     @Override
@@ -57,10 +66,10 @@ public class SignInActivity extends AppCompatActivity {
                 } else {
                     ds.setColor(Color.BLACK);
                 }
-            };
+            }
         };
 
-        SpannableString str = new SpannableString (SIGN_UP_INFO);
+        SpannableString str = new SpannableString(SIGN_UP_INFO);
         str.setSpan(new android.text.style.StyleSpan(android.graphics.Typeface.BOLD), INT_START, INT_END, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
         str.setSpan(clickableSpan, INT_START, INT_END, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
         singUpInfo.setText(str);
@@ -73,65 +82,71 @@ public class SignInActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    private void displayToast() {
+        Toast.makeText(SignInActivity.this, "Error occurred",
+                Toast.LENGTH_LONG).show();
+    }
+
     public void onSignInClick(View view) {
         EditText emailElement = findViewById(R.id.sign_in_email);
         String email = emailElement.getText().toString();
         EditText passwordElement = findViewById(R.id.sign_in_password);
         String password = passwordElement.getText().toString();
-
         boolean emailValid = validateEmail(email);
-      //  boolean passwordValid = validatePassword(password);
         TextView emailInvalidElement = findViewById(R.id.emailInvalid);
         TextView passwordInvalidElement = findViewById(R.id.passwordInvalid);
+        emailInvalidElement.setText("");
+        passwordInvalidElement.setText("");
 
-        if (emailValid) {
-            emailInvalidElement.setText("");
-        } else {
+        if (email.isEmpty())
+            emailInvalidElement.setText(EMPTY_EMAIL);
+        else if (!emailValid)
             emailInvalidElement.setText(INVALID_EMAIL);
-        }
+        if (password.isEmpty())
+            passwordInvalidElement.setText(EMPTY_PASSWORD);
 
-//        if (passwordValid) {
-//            passwordInvalidElement.setText("");
-//        } else {
-//            passwordInvalidElement.setText(INVALID_PASSWORD);
-//        }
+        if (emailValid && !email.isEmpty() && !password.isEmpty())
+            requestToSignInUser(email, password, emailInvalidElement, passwordInvalidElement);
+    }
 
+    private void requestToSignInUser(String email, String password,
+                                     TextView emailInvalidElement, TextView passwordInvalidElement) {
         AlertMeService service = RestAdapter.getAlertMeService();
         Call<ResponseBody> call = service.signIn(new LoginBody(email, password));
         call.enqueue(new Callback<ResponseBody>() {
             @Override
             public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-                if(response.isSuccessful()){
-                    emailInvalidElement.setText("");
-                    passwordInvalidElement.setText("");
+                if (response.isSuccessful())
                     startActivity(new Intent(getApplicationContext(), MapsActivity.class));
+                else {
+                    try {
+                        String json = response.errorBody().string();
+                        JsonParser jsonParser = new JsonParser();
+                        JsonObject root = jsonParser.parse(json).getAsJsonObject();
+                        int errorCode = root.get("errorCode").getAsInt();
+                        String errorMessage = root.get("error").getAsString();
+                        if (errorCode == INCORRECT_LOGIN_CODE)
+                            emailInvalidElement.setText(errorMessage);
+                        else if (errorCode == INCORRECT_PASSWORD_CODE)
+                            passwordInvalidElement.setText(errorMessage);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        displayToast();
+                    }
                 }
-                else if(response.code() == 500)
-                    emailInvalidElement.setText(INVALID_EMAIL);
-                else if(response.code() == 400)
-                    passwordInvalidElement.setText(INVALID_PASSWORD);
+
             }
 
             @Override
             public void onFailure(Call<ResponseBody> call, Throwable t) {
-
+                displayToast();
             }
         });
-
-        //TODO: Credentials validation on server
-//        if (emailValid && passwordValid) {
-//            startActivity(new Intent(this, MapsActivity.class));
-//        }
     }
 
-    public boolean validateEmail(String email) {
-        return !email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    private boolean validateEmail(String email) {
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches();
     }
 
-//    public boolean validatePassword(String password) {
-//        //TODO: Change validation based on database constraints
-//        System.out.println(isOK[0]);
-//        return !password.isEmpty() && isOK[0];
-//    }
 
 }
