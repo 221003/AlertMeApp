1539eb8b29f6005d7182e4e1ec16589673d855a5
majkelw <michalwik@vp.pl>
2021-11-16 22:12:02 +0100

    Added Vote model and request, declared methods for votes in AlertMeService

--- app/src/main/java/com/example/alertmeapp/api/retrofit/AlertMeService.java
+++ app/src/main/java/com/example/alertmeapp/api/retrofit/AlertMeService.java
@@ -1,20 +1,25 @@
 package com.example.alertmeapp.api.retrofit;
 
 import com.example.alertmeapp.api.data.Alert;
+import com.example.alertmeapp.api.data.Vote;
 import com.example.alertmeapp.api.requests.AlertRequest;
 import com.example.alertmeapp.api.data.AlertType;
 import com.example.alertmeapp.api.data.User;
 import com.example.alertmeapp.api.requests.UserSignInRequest;
 import com.example.alertmeapp.api.requests.UserSignUpRequest;
 
+import com.example.alertmeapp.api.requests.VoteRequest;
 import com.example.alertmeapp.api.responses.ResponseMultipleData;
 import com.example.alertmeapp.api.responses.ResponseSingleData;
 
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
 
 public interface AlertMeService {
     @POST("api/users/login")
@@ -32,4 +37,18 @@ public interface AlertMeService {
     @POST("/api/alerts")
     Call<ResponseBody> saveNewAlert(@Body AlertRequest alertRequest);
 
+    @PUT("/api/alerts/{id}")
+    Call<ResponseSingleData<Alert>> updateAlert(@Body AlertRequest alertRequest, @Path("id") Long id);
+
+    @POST("/api/votes")
+    Call<ResponseSingleData<Vote>> createVote(@Body VoteRequest voteRequest);
+
+    @PUT("/api/votes/{id}")
+    Call<ResponseSingleData<Vote>> updateVote(@Body VoteRequest voteRequest, @Path("id") Long id);
+
+    @DELETE("/api/votes/{id}")
+    Call<ResponseBody> deleteVote(@Path("id") Long id);
+
+    @POST("/api/votes/find")
+    Call<ResponseSingleData<Vote>> findVote(@Body VoteRequest voteRequest);
 }
