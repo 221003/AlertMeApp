9c1785eefb5ab7492899c665199ec74d24c0bc04
Piotr Kalota <piotrkalota99@gmail.com>
2021-11-16 15:33:55 +0100

    Add distance_to to details fragment

--- app/src/main/java/com/example/alertmeapp/fragments/AlertDetailsFragment.java
+++ app/src/main/java/com/example/alertmeapp/fragments/AlertDetailsFragment.java
@@ -1,9 +1,12 @@
 package com.example.alertmeapp.fragments;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.location.Address;
 import android.location.Geocoder;
+import android.location.Location;
 import android.media.Image;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -13,6 +16,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
@@ -21,6 +25,8 @@ import com.example.alertmeapp.api.AlertMeService;
 import com.example.alertmeapp.api.RestAdapter;
 import com.example.alertmeapp.api.serverRequest.AlertFullBody;
 import com.example.alertmeapp.api.serverResponse.AlertResponse;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 
 import java.io.IOException;
 import java.util.Base64;
@@ -32,6 +38,12 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class AlertDetailsFragment extends Fragment {
+    private static String[] PERMISSIONS_LOCALIZATION = {
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+    };
+    private static final int REQUEST_LOCATION_CODE = 103;
+
     public AlertDetailsFragment() {}
 
     public static AlertDetailsFragment newInstance(long alertId) {
@@ -96,6 +108,8 @@ public class AlertDetailsFragment extends Fragment {
         authorView.setText(author);
 
         locationView.setText(getAlertAddress(alert.getLatitude(), alert.getLongitude()));
+
+        setDistanceTo(alert.getLatitude(), alert.getLongitude());
     }
 
     private Bitmap getImageBitmap(String image) {
@@ -117,5 +131,27 @@ public class AlertDetailsFragment extends Fragment {
         String[] splitAddress = address.split(",");
 
         return splitAddress[0] + ", " + splitAddress[1];
+    }
+
+    private void setDistanceTo(Double latitude, Double longitude) {
+        FusedLocationProviderClient fusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(PERMISSIONS_LOCALIZATION, REQUEST_LOCATION_CODE);
+        }
+
+        TextView distanceToView = getView().findViewById(R.id.details_distance_to);
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(getActivity(), location -> {
+                            if (location != null) {
+                                Location current = new Location(location);
+                                current.setLatitude(latitude);
+                                current.setLongitude(longitude);
+                                int distance = (int) location.distanceTo(current);
+                                distanceToView.setText(String.valueOf(distance) + " km");
+                            } else {
+                                distanceToView.setText("Unknown");
+                            }
+                        }
+                ).addOnFailureListener(e -> distanceToView.setText("Unknown"));
     }
 }
