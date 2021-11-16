fffdb19610e31b9ff5c3ead7e2fecdeded9e9dc5
Piotr Kalota <piotrkalota99@gmail.com>
2021-11-16 15:31:21 +0100

    Add type, author and location to details fragment

--- app/src/main/java/com/example/alertmeapp/fragments/AlertDetailsFragment.java
+++ app/src/main/java/com/example/alertmeapp/fragments/AlertDetailsFragment.java
@@ -2,6 +2,8 @@ package com.example.alertmeapp.fragments;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.location.Address;
+import android.location.Geocoder;
 import android.media.Image;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -20,7 +22,10 @@ import com.example.alertmeapp.api.RestAdapter;
 import com.example.alertmeapp.api.serverRequest.AlertFullBody;
 import com.example.alertmeapp.api.serverResponse.AlertResponse;
 
+import java.io.IOException;
 import java.util.Base64;
+import java.util.List;
+import java.util.Locale;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -74,15 +79,43 @@ public class AlertDetailsFragment extends Fragment {
 
     private void populateFragmentView(AlertFullBody alert) {
         ImageView imageView = getView().findViewById(R.id.details_image);
+        TextView titleView = getView().findViewById(R.id.details_title);
+        TextView alertTypeView = getView().findViewById(R.id.details_alert_type);
+        TextView authorView = getView().findViewById(R.id.details_author);
+        TextView locationView = getView().findViewById(R.id.details_location);
+
         imageView.setImageBitmap(getImageBitmap(alert.getImage()));
 
-        TextView titleView = getView().findViewById(R.id.details_title);
-
         titleView.setText(alert.getTitle());
+
+        String alertName = alert.getAlertType().getName();
+        alertName = alertName.substring(0, 1).toUpperCase() + alertName.substring(1).toLowerCase();
+        alertTypeView.setText(alertName);
+
+        String author = alert.getUser().getFistName() + " " + alert.getUser().getLastName();
+        authorView.setText(author);
+
+        locationView.setText(getAlertAddress(alert.getLatitude(), alert.getLongitude()));
     }
 
     private Bitmap getImageBitmap(String image) {
         byte[] decodedImage = Base64.getDecoder().decode(image);
         return BitmapFactory.decodeByteArray(decodedImage, 0, decodedImage.length);
+    }
+
+    private String getAlertAddress(Double latitude, Double longitude) {
+        List<Address> addresses = null;
+        Geocoder geocoder = new Geocoder(getContext(), Locale.getDefault());
+
+        try {
+            addresses = geocoder.getFromLocation(latitude, longitude, 1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String address = addresses.get(0).getAddressLine(0);
+        String[] splitAddress = address.split(",");
+
+        return splitAddress[0] + ", " + splitAddress[1];
     }
 }
